fd39f545126ac9051e16870bfdbe0340916fed35
merlox <merloxdixcontrol@gmail.com>
2017-08-23 16:04:29 +0200

    Completed first page with responsive design

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import './../stylus/index.styl'
+import './../stylus/homepage.styl'
 
 class HomePage extends React.Component {
    constructor(props){
@@ -24,6 +25,29 @@ class HomePage extends React.Component {
                   <div className="banner">
                      <h1>Triple Entry <br/>
                         Accounting Demo</h1>
+                  </div>
+
+                  <div className="banner-shadow">
+                     <p>The purpose of this demo is to introduce to the concept of <i>Triple Entry Accounting</i> using <i>Fizcal</i>.
+                        <i> Fizcal</i> will have it's own propriety distributed file system and blockchain but for demonstration
+                        purposes we will be using <i>IPFS</i> to store ledgers and the <i>Etheruem Blockchain</i> to store and implement
+                        the smart contract
+                     </p>
+
+                     <div className="legend-container">
+                        <h2>To run the demo you need</h2>
+                        <p>To be running a web browser running the <a href="https://metamask.io/">MetaMask</a> or <a href="https://github.com/ethereum/mist">Mist plugin</a>.</p>
+                        <p>You will also need to have some ether in your wallet.</p>
+                        <div className="button-container">
+                           <button>Start Demo</button>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div className="video-block">
+                     <h2>If you don't have any ether
+                        you can watch a video demonstration here</h2>
+                     <img src="img/video.png" />
                   </div>
                </div>
             </div>
