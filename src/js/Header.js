8c7e84ba189efc9b2c912c7a517683c3f2b3aa90
merlox <merloxdixcontrol@gmail.com>
2017-08-25 22:14:07 +0200

    Added highlighted categories in the main header

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../stylus/header.styl'
 
 class Header extends React.Component {
    constructor(props){
       super(props)
+
+      this.state = {
+         currentSection: window.location.pathname.split('/')[1]
+      }
    }
 
    render(){
       return (
          <header>
-            <a href="#" className="header-icon" onClick={() => {this.props.handleMenu(true)}}></a>
+            <Link to="#" className="header-icon" onClick={() => {this.props.handleMenu(true)}}></Link>
             <Link to="/"><img src={this.props.subRoute ? '../img/logo.png' : '/img/logo.png'} className="header-logo"/></Link>
 
             <nav>
-               <a href="#">Home</a>
-               <a href="#">ICO</a>
-               <a href="#">Press</a>
-               <a href="#">Development</a>
-               <a href="#">Mobile App</a>
-               <a href="#">Triple Entry</a>
-               <a href="#">Team</a>
-               <a href="#" className="log-out-button">
+               <Link className={this.state.currentSection === undefined ? 'active-header' : ''} to="#">Home</Link>
+               <Link className={this.state.currentSection === 'ico' ? 'active-header' : ''} to="#">ICO</Link>
+               <Link className={this.state.currentSection === 'press' ? 'active-header' : ''} to="#">Press</Link>
+               <Link className={this.state.currentSection === 'development' ? 'active-header' : ''} to="#">Development</Link>
+               <Link className={this.state.currentSection === 'mobile-app' ? 'active-header' : ''} to="#">Mobile App</Link>
+               <Link className={this.state.currentSection === 'triple-entry' ? 'active-header' : ''} to="/triple-entry">Triple Entry</Link>
+               <Link className={this.state.currentSection === 'team' ? 'active-header' : ''} to="#">Team</Link>
+               <Link to="#" className="log-out-button">
                   Log out
                   <img src={this.props.subRoute ? '../img/icon.png' : 'img/icon.png'} className="log-out-button-icon"/>
-               </a>
+               </Link>
             </nav>
 
             <div className="site-cache" onClick={() => {this.props.handleMenu(false)}}></div>
