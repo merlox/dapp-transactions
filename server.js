fd85976a7591a2768fb55c4f0a5fe9772d15d57c
merlox <merunasgrincalaitis@gmail.com>
2019-09-01 17:44:54 +0200

    Added yargs

--- server.js
+++ server.js
@@ -3,16 +3,28 @@ const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
 const fs = require('fs')
-const port = 80
 const ip = 'localhost'
+const yargs = require('yargs')
+const argv = yargs.option('port', {
+    alias: 'p',
+    description: 'Set the port to run this server on',
+    type: 'number',
+}).help().alias('help', 'h').argv
+if(!argv.port) {
+    console.log('Error, you need to pass the port you want to run this application on with npm start -- -p 8001')
+    process.exit(0)
+}
+const port = argv.port
 
 app.use('/', express.static(path.join(__dirname, 'dist/')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use((req, res, next) => {
-   console.log(`${req.method} Request to ${req.originalUrl}`)
-   next()
+app.use('*', (req, res, next) => {
+	// Logger
+	let time = new Date()
+	console.log(`${req.method} to ${req.originalUrl} at ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+	next()
 })
 
 app.get('*/build.js', (req, res) => {
